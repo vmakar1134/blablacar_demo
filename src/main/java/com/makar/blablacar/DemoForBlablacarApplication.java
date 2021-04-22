8e6b92507f92ea545393ec1a414268d3fd9cb697
Makar <vmakar1134@gmail.com>
2021-04-22 16:34:25 +0300

    init project setup

---
+++ src/main/java/com/makar/blablacar/DemoForBlablacarApplication.java
@@ -0,0 +1,13 @@
+package com.makar.blablacar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoForBlablacarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoForBlablacarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/makar/blablacar/DemoForBlablacarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makar.blablacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoForBlablacarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
